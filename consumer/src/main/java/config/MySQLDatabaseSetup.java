ad11885f26e7b5e637e1e6b1f3afbbf0731093de
Yingbing-Zhu <156177894+Yingbing-Zhu@users.noreply.github.com>
2024-07-24 13:57:48 -0700

    Update MySQLDatabaseSetup.java

--- consumer/src/main/java/config/MySQLDatabaseSetup.java
+++ consumer/src/main/java/config/MySQLDatabaseSetup.java
@@ -13,39 +13,39 @@ public class MySQLDatabaseSetup {
     private static final String USERNAME = "admin";
     private static final String PASSWORD = "rootroot";
     private static final int MAX_POOL_SIZE = 1;
+    
+    public static void main(String[] args) {
+       HikariCPConnectionPool connectionPool = new HikariCPConnectionPool(JDBC_URL, USERNAME, PASSWORD, MAX_POOL_SIZE);
 
-//    public static void main(String[] args) {
-//        HikariCPConnectionPool connectionPool = new HikariCPConnectionPool(JDBC_URL, USERNAME, PASSWORD, MAX_POOL_SIZE);
-//
-//        try (Connection baseConn = connectionPool.getConnection();
-//             Statement baseStmt = baseConn.createStatement()) {
-//
-//            // Create Database
-//            String createDatabaseSQL = "CREATE DATABASE IF NOT EXISTS " + DATABASE_NAME;
-//            baseStmt.executeUpdate(createDatabaseSQL);
-//            System.out.println("Database created successfully...");
-//
-//            // Connect to the newly created database
-//            try (Connection dbConn = connectionPool.getConnection(DATABASE_NAME);
-//                 Statement dbStmt = dbConn.createStatement()) {
-//
-//                // Create Table
-//                String createTableSQL = "CREATE TABLE IF NOT EXISTS lift_rides (" +
-//                        "id INT AUTO_INCREMENT PRIMARY KEY," +
-//                        "skier_id INT NOT NULL," +
-//                        "season_id VARCHAR(10) NOT NULL," +
-//                        "day_id VARCHAR(10) NOT NULL," +
-//                        "lift_id INT NOT NULL," +
-//                        "time INT NOT NULL," +
-//                        "resort_id INT NOT NULL)";
-//                dbStmt.executeUpdate(createTableSQL);
-//                System.out.println("Table created successfully...");
-//            }
-//
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        } finally {
-//            connectionPool.close();
-//        }
-//    }
+       try (Connection baseConn = connectionPool.getConnection();
+            Statement baseStmt = baseConn.createStatement()) {
+
+           // Create Database
+           String createDatabaseSQL = "CREATE DATABASE IF NOT EXISTS " + DATABASE_NAME;
+           baseStmt.executeUpdate(createDatabaseSQL);
+           System.out.println("Database created successfully...");
+
+           // Connect to the newly created database
+           try (Connection dbConn = connectionPool.getConnection(DATABASE_NAME);
+                Statement dbStmt = dbConn.createStatement()) {
+
+               // Create Table
+               String createTableSQL = "CREATE TABLE IF NOT EXISTS lift_rides (" +
+                       "id INT AUTO_INCREMENT PRIMARY KEY," +
+                       "skier_id INT NOT NULL," +
+                       "season_id VARCHAR(10) NOT NULL," +
+                       "day_id VARCHAR(10) NOT NULL," +
+                       "lift_id INT NOT NULL," +
+                       "time INT NOT NULL," +
+                       "resort_id INT NOT NULL)";
+               dbStmt.executeUpdate(createTableSQL);
+               System.out.println("Table created successfully...");
+           }
+
+       } catch (SQLException e) {
+           e.printStackTrace();
+       } finally {
+           connectionPool.close();
+       }
+   }
 }
